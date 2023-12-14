d3117fbe792b5d7495d96d423298248f6823ea52
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2023-12-14 22:47:22 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * main - main args
@@ -32,7 +32,7 @@ int main(int ac, char **av, char **env)
 			else
 			{
 				n = _values_path(&user_command[0], env);
-				_exit = _fork_func(user_command, av, env, getcommand, pathValue, n);
+				_exit = _fork_fun(user_command, av, env, getcommand, pathValue, n);
 				if (n == 0)
 					free(user_command[0]);
 			}
