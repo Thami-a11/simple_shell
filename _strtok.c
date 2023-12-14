42bda686fb6fb785075726fdba8860316b62cdac
Thami-a11 <78639094+Thami-a11@users.noreply.github.com>
2023-12-14 22:33:12 +0200

    Create _strtok.c

---
+++ _strtok.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+char *_strchr(const char *s, int c);
+size_t _strspn(const char *s1, const char *s2);
+size_t _strcspn(const char *s1, const char *s2);
+
+/**
+ * _strtok - breaks string 
+ * @str: string to tokenize
+ * @delim: string with the char.
+ * Return: the first/next token if possible, a null-pointer otherwise.
+ **/
+char *_strtok(char *str, const char *delim)
+{
+	static char *p;
+
+	if (str)
+		p = str;
+	else if (!p)
+		return (0);
+	str = p + _strspn(p, delim);
+	p = str + _strcspn(str, delim);
+	if (p == str)
+		return (p = 0);
+	p = *p ? *p = 0, p + 1 : 0;
+	return (str);
+}
+/**
+ * _strcspn - computes the length
+ * @s1: string to check
+ * @s2: string useful to compare
+ * Return: the length of the segment.
+ **/
+size_t _strcspn(const char *s1, const char *s2)
+{
+	size_t ret = 0;
+
+	while (*s1)
+	{
+		if (_strchr(s2, *s1))
+			return (ret);
+		s1++, ret++;
+	}
+	return (ret);
+}
+/**
+ * _strspn - gets the length 
+ * @s1: strint to compute the lengh
+ * @s2: string delimit
+ * Return: the length of the segment.
+ **/
+size_t _strspn(const char *s1, const char *s2)
+{
+	size_t ret = 0;
+
+	while (*s1 && _strchr(s2, *s1++))
+		ret++;
+	return (ret);
+}
+/**
+ * _strchr - locates the ﬁrst occurrence
+ * @s: string
+ * @c: char
+ * Return: a pointer
+ **/
+char *_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+		if (!*s++)
+			return (0);
+	return ((char *)s);
+}
